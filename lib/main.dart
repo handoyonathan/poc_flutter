abde4d16b6e43d76f4c830bde1a89d4bed288752
handoyonathan <yehaccnt@gmail.com>
2024-11-05 17:04:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_poc/E2E_enrcryption/transaction.dart';
+import 'package:flutter_poc/biometric/biometric_auth_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BiometricAuthPage(),
     );
   }
 }
